9dfbef348992dfdeb22bd1519d4e2603a6aa5a2d
R-Grigala <r.grigalashvili777@gmail.com>
2022-12-11 02:21:53 +0400

    Coding Contniue:// 1:06:00

--- src/App.js
+++ src/App.js
@@ -9,8 +9,8 @@ function App() {
   return (
     <>
     <Routes>
-      <Route path="/" element={<Layout/>} >
-        <Route index element={<Home/>} />
+      <Route path="/" element={<Layout />} >
+        <Route index element={<Home />} />
         <Route path='about' element={<About />}/>
       </Route>
     </Routes>
