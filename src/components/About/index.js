97bb07405a9454d1266fd508eb05729d1d9637ff
R-Grigala <r.grigalashvili777@gmail.com>
2022-12-10 23:42:30 +0400

    Coding Continue:// 50:00

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -11,7 +11,20 @@ const About = () => {
                         idx={15}
                     />
                 </h1>
-                <p></p>
+                <p>
+                    I'm a very ambitious front-end developer looking for a role in an
+                    established IT company with the opportunity to work with the latest
+                    technologies on challenging and diverse projects.
+                </p>
+                <p align="LEFT">
+                    I'm quiet confident, naturally curious, and perpetually working on
+                    improving my chops one design problem at a time.
+                </p>
+                <p>
+                    If I need to define myself in one sentence that would be a family
+                    person, father of a beautiful daughter, a sports fanatic,
+                    photography enthusiast, and tech-obsessed!!!
+                </p>
             </div>
         </div>
     )
