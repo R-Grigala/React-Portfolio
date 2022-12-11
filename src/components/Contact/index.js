16b3c739f814abcfb4ad2d029a0beae010123206
R-Grigala <r.grigalashvili777@gmail.com>
2022-12-11 20:37:33 +0400

    Coding Continue:// 1:08:00

---
+++ src/components/Contact/index.js
@@ -0,0 +1,14 @@
+import './index.scss'
+import Loader from 'react-loaders'
+
+const Contact = () => {
+
+    return (
+        <>
+            <div className='container contact-page'></div>
+            <Loader type="pacman"/>
+        </>
+    )
+}
+
+export default Contact
