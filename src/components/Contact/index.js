8d01e500564103a41db252652a21c6c389047274
R-Grigala <r.grigalashvili777@gmail.com>
2022-12-11 21:50:50 +0400

    Coding Contniue:// 1:17:38

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,14 +1,131 @@
-import './index.scss'
+import { useEffect, useState } from 'react'
 import Loader from 'react-loaders'
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css'
+import { useRef } from 'react'
+import emailjs from '@emailjs/browser'
+import AnimatedLetters from '../AnimatedLetters'
+import './index.scss'
 
 const Contact = () => {
+  const [letterClass, setLetterClass] = useState('text-animate')
+  const form = useRef()
 
-    return (
-        <>
-            <div className='container contact-page'></div>
-            <Loader type="pacman"/>
-        </>
-    )
+  useEffect(() => {
+    return setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 3000)
+  }, [])
+
+  const sendEmail = (e) => {
+    e.preventDefault()
+
+    emailjs
+      .sendForm('service_2q9msjd', 'template_bydsl9m', form.current, '2y5pzCnisTfUVtXCb')
+      .then(
+        () => {
+            toast.success('Email has been sent successfully', {
+                position: "top-right",
+                theme: "dark",
+                autoClose: 3000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                });
+            e.target.reset();
+        },
+        () => {
+            toast.error("Dont sent your email", {
+                position: "top-right",
+                autoClose: 3000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                });        
+            }
+      )
+  }
+
+  return (
+    <>
+        <ToastContainer />
+        <div className="container contact-page">
+            <div className="text-zone">
+            <h1>
+                <AnimatedLetters
+                letterClass={letterClass}
+                strArray={['C', 'o', 'n', 't', 'a', 'c', 't', ' ', 'm', 'e']}
+                idx={15}
+                />
+            </h1>
+            <p>
+                I am interested in freelance opportunities - especially on ambitious
+                or large projects. However, if you have any other requests or
+                questions, don't hesitate to contact me using below form either.
+            </p>
+            <div className="contact-form">
+                <form ref={form} onSubmit={sendEmail}>
+                <ul>
+                    <li className="half">
+                    <input placeholder="Name" type="text" name="name" required />
+                    </li>
+                    <li className="half">
+                    <input
+                        placeholder="Email"
+                        type="email"
+                        name="email"
+                        required
+                    />
+                    </li>
+                    <li>
+                    <input
+                        placeholder="Subject"
+                        type="text"
+                        name="subject"
+                        required
+                    />
+                    </li>
+                    <li>
+                    <textarea
+                        placeholder="Message"
+                        name="message"
+                        required
+                    ></textarea>
+                    </li>
+                    <li>
+                    <input type="submit" className="flat-button" value="SEND" />
+                    </li>
+                </ul>
+                </form>
+            </div>
+        </div>
+        {/* <div className="info-map">
+          Slobodan Gajić,
+          <br />
+          Serbia,
+          <br />
+          Branka RadiČevića 19, 22000 <br />
+          Sremska Mitrovica <br />
+          <br />
+          <span>freelancerslobodan@gmail.com</span>
+        </div>
+        <div className="map-wrap">
+          <MapContainer center={[44.96366, 19.61045]} zoom={13}>
+            <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+            <Marker position={[44.96366, 19.61045]}>
+              <Popup>Sloba lives here, come over for a cup of coffee :)</Popup>
+            </Marker>
+          </MapContainer>
+        </div> */}
+      </div>
+      <Loader type="pacman" />
+    </>
+  )
 }
 
 export default Contact
